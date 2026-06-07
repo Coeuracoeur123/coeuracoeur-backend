68272157db9f4a90f0df4f824cbac0097dd1a935
Xynorash <nashtefison@gmail.com>
2026-06-07 12:10:41 +0300

    refactor: add src/routes/admin.js and routes/index.js

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use(require('./auth'));
+router.use(require('./users'));
+router.use(require('./projects'));
+router.use(require('./donations'));
+router.use(require('./messages'));
+router.use(require('./admin'));
+
+module.exports = router;
